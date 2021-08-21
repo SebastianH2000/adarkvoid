92ef09e4fbc17a2eb81b4d0e4f244a58e0599622
SebastianH2000 <87954095+SebastianH2000@users.noreply.github.com>
2021-08-21 12:45:45 -0400

    Add files via upload

---
+++ javascripts/scaleWindow.js
@@ -0,0 +1,9 @@
+function scaleWindow() {
+    var win = window,
+    doc = document,
+    docElem = doc.documentElement,
+    body = doc.getElementsByTagName('body')[0],
+    x = win.innerWidth || docElem.clientWidth || body.clientWidth,
+    y = win.innerHeight|| docElem.clientHeight|| body.clientHeight;
+    document.getElementById("gameWindow").style.transform = "scale(" + x/1250 + ")";
+}
