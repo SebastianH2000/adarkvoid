3e657dae2bbda5183a28607c01aab3425d0c079b
SebastianH2000 <87954095+SebastianH2000@users.noreply.github.com>
2021-08-27 22:27:00 -0400

    Update loop.js

--- javascripts/loop.js
+++ javascripts/loop.js
@@ -6,7 +6,7 @@ let elapsedTime = Date.now() - currentTime;
 function mainLoop() {
   elapsedTime += Date.now() - currentTime;
   currentTime = Date.now();
-  if (elapsedTime > 500) {elapsedTime = 500};
+  if (elapsedTime > 120000) {elapsedTime = 120000};
   while(elapsedTime > 20) {
     elapsedTime -= 20;
     //do stuff
